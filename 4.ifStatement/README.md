097cf240a0533099d3f1f3df997d4d7f0234a21d
EstherUduma <estheruduma23@gmail.com>
2023-07-21 11:50:00 +0100

    update

---
+++ 4.ifStatement/README.md
@@ -0,0 +1,8 @@
+<h1>IF STATEMENT</h1>
+
+<h2>Description</h2>
+The if statement helps us test the condition.
+
+<h2>Environments Used </h2>
+- <b>Visual Studio Code</b>
+
---
+++ 4.ifStatement/ifStatement.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i = 6;
+    if (i == 6)
+    {
+        char c = 'c';
+        printf("%c", c);
+    }
+    
+    return (0);
+}
